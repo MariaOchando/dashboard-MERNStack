e2a2c24d053c4d5d39051ea2c389032319d57fec
Maria <mariaochandocalvo@gmail.com>
2023-03-17 15:25:20 +0000

    properties display

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Add } from '@mui/icons-material';
+import { useList } from '@pankod/refine-core';
+import { Box, Stack, Typography } from '@pankod/refine-mui';
+import { useNavigate } from '@pankod/refine-react-router-v6';
+import { PropertyCard, CustomButton } from 'components';
 
-const allProperties = () => {
+const AllProperties = () => {
+  const navigate = useNavigate();
   return (
-    <div>all-properties</div>
+    
   )
 }
 
-export default allProperties
+export default AllProperties
